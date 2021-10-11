8c5897d473ece224924fd7502c835d99b1361cea
Alejandro Hernández <hernandezlara180@gmail.com>
2021-10-11 14:05:03 -0600

    clientesAdmin correccion

--- ProyectoLaravel/resources/views/clientesAdmin.blade.php
+++ ProyectoLaravel/resources/views/clientesAdmin.blade.php
@@ -3,36 +3,30 @@
 @section('titulo','Clientes')
 
 @section('contenido')
-
 	<div class="section-title text-center">
 		<br><h3>Listado de clientes</h3>
 	</div>
-
-
-		<table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
-			<thead>
+	
+	<table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
+		<thead>
+			<tr>
+				<th>Código del Cliente</th>
+				<th>Nombre del Cliente</th>
+				<th>Apellidos</th>
+				<th>Correo</th>
+			</tr>
+		</thead>
+		<tbody>
+			@forelse ($clientesAdmin as $clienteAdminItem)
 				<tr>
-					<th>Código del Cliente</th>
-					<th>Nombre del Cliente</th>
-					<th>Apellidos</th>
-					<th>Correo</th>
+					<td>{{$clienteAdminItem->id}}</td>
+					<td>{{$clienteAdminItem->nombre}}</td>
+					<td>{{$clienteAdminItem->apellidos}}</td>
+					<td>{{$clienteAdminItem->correo}}</td>	
 				</tr>
-			</thead>
-			<tbody>
-				<tr>
-
-					@forelse ($clientesAdmin as $clienteAdminItem)
-						
-							<td>{{$clienteAdminItem->id}}</td>
-							<td>{{$clienteAdminItem->nombre}}</td>
-							<td>{{$clienteAdminItem->apellidos}}</td>
-							<td>{{$clienteAdminItem->correo}}</td>	
-						
-					@empty		
-						No hay datos que mostrar.			
-					@endforelse
-				</tr>			
-			</tbody>
-		</table>
-
+			@empty
+					No hay datos que mostrar.			
+			@endforelse
+		</tbody>
+	</table>
 @endsection
